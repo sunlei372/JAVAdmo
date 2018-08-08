ff90204fe121d7fc37c9b4046d172c8910dd8dbb
sunlei <121648522@qq.com>
2018-08-08 14:09:15 +0800

    测试提交

---
+++ ASA080727/src/cn/sinomdt/sn/oop/main/AsaDemo.java
@@ -0,0 +1,24 @@
+package cn.sinomdt.sn.oop.main;
+
+public class AsaDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("开始你的表演");
+		People people = new People();
+		people.Name = "张三";
+		people.Age = 18;
+		people.Eatting(people);
+		people.Look(people, "看足球赛");
+		people.sleep(people);
+		People people2 = new People();
+		people2.Name = "李四";
+		people2.Age = 45;
+		people2.Smoking(people2);
+		people2.Eatting(people2);
+   
+		System.out.println("表演结束1");
+
+	}
+
+}
